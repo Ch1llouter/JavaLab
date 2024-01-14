03b19e82e8cb7aa33b01c9306bd1eed5fd28486a
Ch1llouter <122366812+Ch1llouter@users.noreply.github.com>
2024-01-14 10:38:15 +0200

    Add files via upload
    Tests for same lab 4.1 4.2

---
+++ java/org/dut/lab42/DecoderTests.java
@@ -0,0 +1,8 @@
+package org.dut.lab42;
+
+class DecoderTests {
+    public static void main(String[] args) {
+        String message = "dfsdf4 resr1 esse e2sef33";
+        System.out.print(Decoder.decodeEncodedString(message));
+    }
+}
